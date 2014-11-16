af756401a9b37096c730afe3e0c41e55e365c097
Sirius <sirius@Siriuss-MacBook-Air.local>
2014-11-16 12:19:32 -0800

    Working example.

--- app.js
+++ app.js
@@ -8,8 +8,37 @@ var server = app.listen(3000, function() {
 
 var io = require('socket.io')(server);
 
-// Syc logic
+// Syc logic - Creating an array of messages to by synchronized
 var messages = syc.sync('messages', []);
+
+syc.verify(syc.list('messages'), function (changes, socket) { 
+  var change = changes.change,
+      properties = getProperties(change);
+
+  if (typeof change !== 'object') {
+    return false;
+  }
+  if (properties.length !== 2) {
+    return false;
+  }
+  if (!change.content || !change.screenname) {
+    return false;
+  }
+  if (typeof change.content !== 'string' && typeof change.screenname !== 'string') {
+    return false;
+  }
+
+  return true;
+
+  function getProperties (object) { 
+    var properties = [];
+    for (var property in object) {
+      properties.push(property);
+    }
+    return properties;
+  }
+});
+console.log(syc.verifiers);
 
 // Routes
 app.get('/', function(req, res){
